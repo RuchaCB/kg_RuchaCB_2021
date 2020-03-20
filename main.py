6238e822671e50114b2ccb32eb03ae6632ec3e4c
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-03-20 03:30:40 -0400

    Update main.py
    added docstring to explain the project functionality

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+run this file to see the project output. It takes 2 strings as user input to check whether these two strings can be mapped or not.
+returns true if can and returns false if cannot
+"""
 from mapping import validate
 
 if __name__ == '__main__':
