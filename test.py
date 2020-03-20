577215bed555ec093ca8cf758597b0877c13cd6f
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-03-20 03:25:07 -0400

    Update test.py
    increased readability of code by making some changes suggested by pylint
    changed return_value to test user input taken by validate()

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
+"""To test all the functions from mapping.py"""
 import unittest
 from unittest.mock import patch
 from mapping import mapping, validate
 
 class TestMapping(unittest.TestCase):
-    
-    def testMapping(self):
-        self.assertEqual("true", mapping("abc","bcd"))
-        self.assertEqual("false", mapping("foo","bar"))
-        self.assertEqual("true", mapping("bar","foo"))
-        self.assertEqual("true", mapping("123123","456456"))
-        self.assertEqual("true", mapping("123","abcdef"))
-        self.assertEqual("false", mapping("123456","123"))
-    
-    @patch('mapping.input', return_value='Please enter a valid input')
-    def test_validate(self, input):
+    """To test the mapping.py"""
+    def test_mapping(self):
+        """To test the mapping() function"""
+        self.assertEqual("true", mapping("abc", "bcd"))
+        self.assertEqual("false", mapping("foo", "bar"))
+        self.assertEqual("true", mapping("bar", "foo"))
+        self.assertEqual("true", mapping("123123", "456456"))
+        self.assertEqual("true", mapping("123", "abcdef"))
+        self.assertEqual("false", mapping("123456", "123"))
+
+    @patch('mapping.input', return_value='123 456 678')
+    def test_validate(self, inp):
+        """To test the validate() function"""
         self.assertEqual(validate(), 'Please enter a valid input')
 
 
