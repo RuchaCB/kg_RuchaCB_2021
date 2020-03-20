94e5ea1624a29ad0eacf011af4a17b51ab726b88
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-03-20 03:27:56 -0400

    Update mapping.py
    made changes suggested by pylint
    optimized validate() function which also result in improved code-coverage; removed NameError

--- mapping.py
+++ mapping.py
@@ -1,12 +1,16 @@
-
+"""
+contains 2 functions
+mapping() which is called inside validate()
+"""
 def mapping(string1, string2):
-    dict1={}
-    if len(string2)<len(string1):
+    """ To check whether the string1 can be mapped to string2 or not"""
+    dict1 = {}
+    if len(string2) < len(string1):
         flag = "false"
     else:
         for i, char in enumerate(string1):
             if char not in dict1:
-                dict1[char]=string2[i]
+                dict1[char] = string2[i]
                 flag = "true"
             elif dict1[char] == string2[i]:
                 flag = "true"
@@ -15,10 +19,10 @@ def mapping(string1, string2):
     return flag
 
 def validate():
+    """To take valid user input"""
     try:
-        string1, string2 = input().split() 
-        print(mapping(string1,string2))
+        string1, string2 = input().split()
+        output = mapping(string1, string2)
     except ValueError:
-        return ("Please enter a valid input")
-    except NameError:
-        return ("Please enter a valid input")
+        output = "Please enter a valid input"
+    return output
